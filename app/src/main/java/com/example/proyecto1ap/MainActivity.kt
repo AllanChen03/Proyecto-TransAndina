5fea708fc6a578776c2a59a9addbf9ee049e8c2b
kolbejarano <Krbejarano@estudantec.cr>
2026-09-15 22:31:44 -0600

    Agregar pantalla de login y registro

--- app/src/main/java/com/example/proyecto1ap/MainActivity.kt
+++ app/src/main/java/com/example/proyecto1ap/MainActivity.kt
@@ -11,14 +11,25 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.example.proyecto1ap.ui.theme.Proyecto1APTheme
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.providers.builtin.Email
@@ -32,7 +43,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Proyecto1APTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PantallaPrueba(modifier = Modifier.padding(innerPadding))
+                    PantallaInicial(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -40,64 +51,225 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PantallaPrueba(modifier: Modifier = Modifier) {
-    val scope = rememberCoroutineScope()
+fun PantallaInicial(modifier: Modifier= Modifier) {
+    val scope= rememberCoroutineScope()
+
+    var correo by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var mensaje by remember { mutableStateOf("") }
 
     Column(
-        modifier = modifier
+        modifier= modifier
             .fillMaxSize()
             .padding(24.dp)
     ) {
         Text(
-            text = "Flotilla TransAndina",
-            fontSize = 24.sp
+            text= "TransAndina",
+            fontSize= 28.sp,
+            fontWeight= FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = {
-            scope.launch {
-                try {
-                    val correo = "allan.conductor1@gmail.com"
 
-                    SupabaseManager.client.auth.signUpWith(Email) {
-                        email = correo
-                        password = "test123456"
+        Text("Correo electrónico")
+        OutlinedTextField(
+            value= correo,
+            onValueChange= { nuevoTexto ->
+                correo= nuevoTexto
+            }
+        )
+
+        Text("Contraseña")
+        OutlinedTextField(
+            value= password,
+            onValueChange= { nuevoTexto ->
+                password= nuevoTexto
+            },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Button(
+            onClick= {
+                scope.launch {
+                    try {
+                        SupabaseManager.client.auth.signInWith(Email) {
+                            email= correo
+                            password= password
+                        }
+                        mensaje= "Inicio de sesión exitoso"
+                    } catch (e: Exception) {
+                        mensaje= "Correo o contraseña incorrectos"
                     }
-
-                    val userId = SupabaseManager.client.auth.currentUserOrNull()?.id
-                    Log.d("PRUEBA", "Auth creado, id: $userId")
-
-                    if (userId == null) {
-                        Log.e("PRUEBA", "No hay sesión activa")
-                        return@launch
-                    }
-
-                    val perfil = Usuario(
-                        id = userId,
-                        nombreCompleto = "Allan Conductor",
-                        cedula = "112340567",
-                        correo = correo,
-                        telefono = "88887777",
-                        numeroLicencia = "B1-12345",
-                        rol = "CONDUCTOR"
-                    )
-
-                    SupabaseManager.client.from("usuarios").insert(perfil)
-                    Log.d("PRUEBA", "Perfil insertado")
-
-                } catch (e: Exception) {
-                    Log.e("PRUEBA", "Error: ${e.message}", e)
                 }
             }
-        }) {
-            Text("Crear usuario de prueba")
+        ) {
+            Text("Ingresar")
         }
+
+        Text(mensaje)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PantallaRegistro(modifier: Modifier = Modifier){
+    val scope = rememberCoroutineScope()
+
+    var nombre_completo by remember { mutableStateOf("") }
+    var cedula by remember { mutableStateOf("") }
+    var correo by remember { mutableStateOf("") }
+    var telefono by remember { mutableStateOf("")}
+    var rol by remember { mutableStateOf("CONDUCTOR")}
+    var expanded by remember { mutableStateOf(false) }
+    var LicenciaNum by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("") }
+    var mensaje by remember { mutableStateOf("") }
+
+    val roles = listOf("CONDUCTOR", "MECANICO", "ENCARGADO")
+    Column(
+        modifier= modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        Text(
+            text= "Registro",
+            fontSize= 28.sp,
+            fontWeight= FontWeight.Bold
+        )
+
+        Text("Nombre completo")
+        OutlinedTextField(
+            value= nombre_completo,
+            onValueChange= { nuevoTexto ->
+                nombre_completo= nuevoTexto
+            }
+        )
+
+        Text("Cédula")
+        OutlinedTextField(
+            value= cedula,
+            onValueChange= { nuevoTexto ->
+                cedula= nuevoTexto
+            }
+        )
+
+        Text("Correo electrónico")
+        OutlinedTextField(
+            value= correo,
+            onValueChange= { nuevoTexto ->
+                correo= nuevoTexto
+            }
+        )
+
+        Text("Teléfono")
+        OutlinedTextField(
+            value= telefono,
+            onValueChange= { nuevoTexto ->
+                telefono= nuevoTexto
+            }
+        )
+        Text("Rol")
+        ExposedDropdownMenuBox(
+            expanded= expanded,
+            onExpandedChange= {
+                expanded= !expanded
+            }
+        ) {
+            OutlinedTextField(
+                value= rol,
+                onValueChange= {},
+                readOnly= true,
+                label= { Text("") },
+                trailingIcon= {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded= expanded)
+                },
+                modifier= Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded= expanded,
+                onDismissRequest= {
+                    expanded= false
+                }
+            ) {
+                roles.forEach { opcion ->
+                    DropdownMenuItem(
+                        text= {
+                            Text(opcion)
+                        },
+                        onClick = {
+                            rol= opcion
+                            expanded= false
+                        }
+                    )
+                }
+            }
+        }
+
+        if (rol=="CONDUCTOR"){
+            Text("Número de licencia")
+            OutlinedTextField(
+                value= LicenciaNum,
+                onValueChange= { nuevoTexto ->
+                    LicenciaNum= nuevoTexto
+                },
+                visualTransformation = PasswordVisualTransformation()
+            )
+        }
+
+        Text("Contraseña")
+        OutlinedTextField(
+            value= password,
+            onValueChange= { nuevoTexto ->
+                password= nuevoTexto
+            },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Button(
+            onClick= {
+                scope.launch {
+                    try {
+                        SupabaseManager.client.auth.signUpWith(Email) {
+                            email = correo
+                            password = password
+                        }
+
+                        val userId = SupabaseManager.client.auth.currentUserOrNull()?.id
+
+                        if (userId == null) {
+                            Log.e("PRUEBA", "No hay sesión activa")
+                            return@launch
+                        }
+
+                        val perfil= Usuario(
+                            id= userId,
+                            nombreCompleto= nombre_completo,
+                            cedula= cedula,
+                            correo= correo,
+                            telefono= telefono,
+                            numeroLicencia= if (rol == "CONDUCTOR") LicenciaNum else null,
+                            rol= rol,
+                            estado= "ACTIVO"
+                        )
+                        SupabaseManager.client.from("usuarios").insert(perfil)
+                        mensaje= "Usuario registrado correctamente"
+                    } catch (e: Exception) {
+                        mensaje= "Error al registrar: ${e.message}"
+                    }
+                }
+            }
+        ) {
+            Text("Ingresar")
+        }
+
+        Text(mensaje)
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Proyecto1APTheme {
-        PantallaPrueba()
+        PantallaRegistro()
     }
 }
